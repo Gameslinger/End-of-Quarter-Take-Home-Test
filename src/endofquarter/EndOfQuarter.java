11a428195dcdee675d9aa984283d6f8c8afd51bf
student <student@172.26.3.192>
2016-10-06 11:30:10 -0600

    Added more stuff!!!

--- src/endofquarter/EndOfQuarter.java
+++ src/endofquarter/EndOfQuarter.java
@@ -8,6 +8,7 @@ package endofquarter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -20,6 +21,7 @@ public class EndOfQuarter {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Page One:
         List<Double> numbers = new ArrayList();
         
         Scanner input = new Scanner(System.in);
@@ -46,5 +48,23 @@ public class EndOfQuarter {
           sum += dbl;
         }
         System.out.println("The sum is: "+sum);
+        System.out.println("\n");
+        
+        //AP requirement
+        int radiusEntry;
+        
+        double area;
+        do{
+        radiusEntry = Integer.parseInt(JOptionPane.showInputDialog("Give me an Integer"));
+        area = MathFunctions.AreaOfCircle(radiusEntry);
+        if(area == -1){
+            JOptionPane.showMessageDialog(null, "Input another radius","Error",JOptionPane.INFORMATION_MESSAGE);
+        }else{
+            break;
         }
+        }while(true);
+        
+        
+    }
+    
 }
--- src/endofquarter/MathFunctions.java
+++ src/endofquarter/MathFunctions.java
@@ -12,7 +12,11 @@ package endofquarter;
 public class MathFunctions {
     public static double SquareRootAndAddThree(double input){
         return Math.sqrt(input)+3;
-        
-        
+    }
+    public static double AreaOfCircle(int num){
+     if(num < 0)return -1;
+     
+     return Math.PI * num * num;
+     
     }
 }
